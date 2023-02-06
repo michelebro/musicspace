b30101a7262913550b115ff98cd603dd97677287
michelebro <mibrowder94@gmail.com>
2023-02-05 20:44:55 -0800

    wip: routes working

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 const express = require('express');
-const PORT = 3000;
+const mongoose = require('mongoose');
 const app = express();
-const Musicspace = require('./models/musicspace.js');
+const Songs = require('./models/music.js');
+//const methodOverride = require('method-override');
+
+require('dotenv').config(); // looks for a .env file and makes it's vars available to process.env
+const PORT = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
+
+
+mongoose.set('strictQuery', false);
+mongoose.connect(DATABASE_URL);
+
+const db = mongoose.connection;
+
+db.on('connected', () => {
+    console.log('Connected to MongoDB');
+});
+
+db.on('error', (err) => {
+    console.log('An error occurred with MongoDB: ' + err.message);
+});
 
 //Middleware
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
-app.use(methodOverride('_method'));
+//app.use(methodOverride('_method'));
 
-//INDEX
+//INDEXnod
 app.get('/musicspace/', (req, res) => {
     res.render('index.ejs', {
         allSongs : Songs
@@ -22,19 +41,19 @@ app.get('/musicspace/new',(req, res) => {
 
 //DELETE
 app.delete('/musicspace/:index', (req,res) => {
-    Musicspace.splice(req.params.index, 1);
+    MusicSpace.splice(req.params.index, 1);
     res.redirect('/musicspace');
 });
 
 //UPDATE
 app.put('/musicspace/:index/', (req, res) => {
-    Musicspace[req.params.index] = req.body;
+    MusicSpace[req.params.index] = req.body;
     res.redirect('/musicspace');
 });
 
 //CREATE
 app.get('/musicspace/', (req, res) => {
-    Musicspace.push(req.body);
+    MusicSpace.push(req.body);
     res.redirect('/musicspace');
 });
 
@@ -45,10 +64,10 @@ app.get('/musicspace/:index/edit', (req, res) => {
 
 //SHOW
 app.get('/musicspace/:index', (req, res) => {
-    res.render('show.ejs', { Musicspace : Musicspace[req.params.index]});
+    res.render('show.ejs', { Songs : Songs[req.params.index]});
 });
 
 //LISTEN
 app.listen(PORT, () => {
-    console.lof(`Server is listening on ${PORT}`);
+    console.log(`Server is listening on ${PORT}`);
 });
